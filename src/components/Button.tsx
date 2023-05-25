3eab2774a81bf7b63c19198efde78e8983467200
Bibash Thapa Magar <be2018se635@gces.edu.np>
2023-05-25 17:59:24 +0545

    [add]: custom-button

---
+++ src/components/Button.tsx
@@ -0,0 +1,41 @@
+import React from 'react';
+
+type propsType = {
+  type: 'button' | 'submit' | 'reset' | undefined;
+  colorScheme: string;
+  handleClick: React.MouseEventHandler;
+  children: React.ReactNode;
+};
+
+const Button = ({
+  type = 'button',
+  colorScheme = '',
+  handleClick = () => {},
+  children,
+}: propsType) => {
+  const getColorScheme = (color: string) => {
+    switch (color) {
+      case 'green':
+        return 'bg-green-600 hover:bg-green-700';
+
+      case 'red':
+        return 'bg-red-600 hover:bg-red-700';
+
+      default:
+        return 'bg-blue-600 hover:bg-blue-700';
+    }
+  };
+
+  return (
+    <button
+      type={type}
+      className={`font-sans text-white font-medium rounded-lg text-sm px-5 py-2.5 focus:outline-none ${getColorScheme(
+        colorScheme
+      )}`}
+    >
+      {children}
+    </button>
+  );
+};
+
+export default Button;
