b1bb8e21b39eb77c7e7615cf117c5b9d9f57d58b
Bibash Thapa Magar <be2018se635@gces.edu.np>
2023-05-25 21:25:26 +0545

    [add]: utils

---
+++ client/src/utils/projectStalls.ts
@@ -0,0 +1,23 @@
+export const projectStalls = [
+  {
+    id: 'sdbj4168',
+    name: 'Woitr',
+    college: 'GCES',
+    stallNumber: 99,
+    cover: '',
+  },
+  {
+    id: 'sdbj416sdf8',
+    name: 'Routine Generation',
+    college: 'GCES',
+    stallNumber: 85,
+    cover: '',
+  },
+  {
+    id: 'sbfddbj4168',
+    name: 'Runner Game',
+    college: 'GCES',
+    stallNumber: 41,
+    cover: '',
+  },
+];
