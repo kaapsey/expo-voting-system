66189d502fa9114d0ca57db6e00dc4d83a7605c5
Bibash Thapa Magar <be2018se635@gces.edu.np>
2023-05-27 12:54:16 +0545

    [update]: scanner page

--- client/src/pages/Scan.tsx
+++ client/src/pages/Scan.tsx
@@ -1,47 +1,50 @@
-import { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
+import { QrScanner } from '@yudiel/react-qr-scanner';
 
 // components
 import Button from '../components/Button';
-import Spinner from '../components/Spinner';
-
-import Html5QrcodePlugin from '../utils/HTML5QrcodePlugin.jsx';
 
 const Scan = () => {
   const navigate = useNavigate();
-  const [isLoading, setIsLoading] = useState(false);
 
   const handleCancel = () => {
     navigate(-1);
   };
 
-  const onNewScanResult = (decodedText: any, decodedResult: any) => {
-    console.log(decodedText, decodedResult);
+  const handleScannerResult = (result: string) => {
+    if (result) {
+      navigate('/vote');
+    }
+  };
+
+  const handleScannerError = (error: any) => {
+    console.log(error);
   };
 
   return (
     <main className='w-screen h-screen bg-gray-800 flex justify-center relative'>
-      {isLoading ? (
-        <div className='flex w-full h-full justify-center items-center'>
-          <Spinner />
-        </div>
-      ) : (
-        <div className='flex flex-col gap-8 w-full items-center px-4 md:px-0 pt-8 md:pt-12 pb-28'>
-          <p className='text-white font-sans font-semibold text-xl text-center'>
-            Show your QR code to start voting
-          </p>
-          {/* <div className='border-2 border-dashed border-red-500 w-2/3 h-5/6 rounded-lg'></div> */}
-          <Html5QrcodePlugin
-            fps={10}
-            qrbox={100}
-            disableFlip={false}
-            qrCodeSuccessCallback={onNewScanResult}
+      <div className='flex flex-col gap-8 w-full items-center px-4 md:px-0 pt-8 md:pt-12 pb-28'>
+        <p className='text-white font-sans font-semibold text-xl text-center'>
+          Show your QR code to start voting
+        </p>
+
+        <div className='w-[300px] md:w-[500px]'>
+          <QrScanner
+            onDecode={(result) => handleScannerResult(result)}
+            onError={(error) => handleScannerError(error)}
+            hideCount={true}
+            containerStyle={{
+              borderRadius: '8px',
+            }}
+            videoStyle={{
+              transform: 'rotateY(180deg)',
+            }}
           />
-          <Button handleClick={handleCancel} colorScheme='red'>
-            Cancel
-          </Button>
         </div>
-      )}
+        <Button handleClick={handleCancel} colorScheme='red'>
+          Cancel
+        </Button>
+      </div>
     </main>
   );
 };
